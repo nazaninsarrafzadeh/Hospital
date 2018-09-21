6b822d7b5077a514fa71398ae626e6e4c0ff9ae3
Fatima <sf.mirashrafi@gmail.com>
2018-09-21 12:44:34 +0430

    AbtUs added

--- app/src/main/java/com/example/nazanin/hospitalapp/AboutUs.java
+++ app/src/main/java/com/example/nazanin/hospitalapp/AboutUs.java
@@ -13,7 +13,14 @@ import android.widget.TextView;
 public class AboutUs extends Fragment {
 
 
-    TextView tv;
+    String text;
+
+    public void SetTitle(String para)
+    {
+        this.text = para;
+    }
+    public TextView tvAbtUs;
+
 
     public static AboutUs newInstance() {
         AboutUs fragment = new AboutUs();
@@ -30,8 +37,29 @@ public class AboutUs extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about_us, container, false);
+        View v= inflater.inflate(R.layout.fragment_about_us, container, false);
+        tvAbtUs = v.findViewById(R.id.textAbtUs);
+        tvAbtUs.setText("این نرم افزار در راستای آموزش پرسنل پرستاری و هم چنین آموزش به بیماران مراجعه کننده به بیمارستان ها و سنجه های اعتبار بخشی معاونت درمان و آموزش پزشکی در سال ۱۳۹۷ تهیه شد.\n" +
+                "این نرم افزار توسط مرکز آموزشی، پژوهشی و درمانی امام حسن(ع) بجنورد جهت پرسنل پرستاری و بخش های بالینی ارائه گردیده است. \n" +
+                "\n" +
+                "این نرم افزار به همت \n" +
+                "دکتر فاطمه خراشادی زاده\n" +
+                "دکتر رامین حقیقی\n" +
+                "سالارپوربرات\n" +
+                "دکتر محمد باقر اوغازیان\n" +
+                "دکتر حبیبه سادات شاکری\n" +
+                "دکتر امیرعلی قهرمانی\n" +
+                "زهرا جوانوش\n" +
+                "لاله زار حسین زاده\n" +
+                "نازنین صراف زاده قدیمی\n" +
+                "سیده فاطمه میراشرفی\n" +
+                "تهیه شده است\n" +
+                "\n" +
+                "باتشکر\n");
+        return v;
     }
 
 }
