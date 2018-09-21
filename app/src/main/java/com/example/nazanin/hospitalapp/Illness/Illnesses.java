b2ded5ea6b13acd341153311eb9435039fac4d00
nazaninsarrafzadeh <nsarrafzade@yahoo.com>
2018-09-21 13:39:47 +0430

    taghriban kamel

--- app/src/main/java/com/example/nazanin/hospitalapp/Illness/Illnesses.java
+++ app/src/main/java/com/example/nazanin/hospitalapp/Illness/Illnesses.java
@@ -6,15 +6,18 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.nazanin.hospitalapp.R;
 
 
 public class Illnesses extends Fragment {
 
-    Button orjans_btn, cheshm_btn, dakheli_btn, ofuni_btn, fogh_btn, ghalb_btn, norology_btn, ccu_btn, icu_btn;
-
+    private ListView illnesses;
+    private String[] list={"اورژانس","چشم","داخلی","عفونی","فوق تخصص","قلب","نورولوژی","CCU","ICU"};
     public static Illnesses newInstance() {
         Illnesses fragment = new Illnesses();
         return fragment;
@@ -30,134 +33,104 @@ public class Illnesses extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_illnesses, container, false);
-
-        orjans_btn = v.findViewById(R.id.btn_orjans);
-        cheshm_btn = v.findViewById(R.id.btn_cheshm);
-        dakheli_btn = v.findViewById(R.id.btn_dakheli);
-        ofuni_btn = v.findViewById(R.id.btn_ofuni);
-        fogh_btn = v.findViewById(R.id.btn_fogh);
-        ghalb_btn = v.findViewById(R.id.btn_ghalb);
-        norology_btn = v.findViewById(R.id.btn_norologi);
-        ccu_btn = v.findViewById(R.id.btn_ccu);
-        icu_btn = v.findViewById(R.id.btn_icu);
-
-        orjans_btn.setOnClickListener(new View.OnClickListener() {
+        illnesses=v.findViewById(R.id.illnesses);
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(getContext(),R.layout.list_textview,list);
+        illnesses.setAdapter(adapter);
+        illnesses.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                IllnessOrjans fragment = new IllnessOrjans();
-                transaction.replace(R.id.frame_layout, fragment);
-                transaction.addToBackStack(null);
-                transaction.commit();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+
+                    case 0:
+
+                        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                        IllnessOrjans fragment = new IllnessOrjans();
+                        transaction.replace(R.id.frame_layout, fragment);
+                        transaction.addToBackStack(null);
+                        transaction.commit();
+                        break;
+
+
+                    case 1:
+
+                        FragmentTransaction transaction1 = getFragmentManager().beginTransaction();
+                        IllnessEye fragment1 = new IllnessEye();
+                        transaction1.replace(R.id.frame_layout, fragment1);
+                        transaction1.addToBackStack(null);
+                        transaction1.commit();
+                        break;
+
+
+                    case 2:
+                        FragmentTransaction transaction2 = getFragmentManager().beginTransaction();
+                        IllnessDakheli fragment2 = new IllnessDakheli();
+                        transaction2.replace(R.id.frame_layout, fragment2);
+                        transaction2.addToBackStack(null);
+                        transaction2.commit();
+                        break;
+
+
+                    case 3:
+                        FragmentTransaction transaction3 = getFragmentManager().beginTransaction();
+                        IllnessOfuni fragment3 = new IllnessOfuni();
+                        transaction3.replace(R.id.frame_layout, fragment3);
+                        transaction3.addToBackStack(null);
+                        transaction3.commit();
+                        break;
+
+
+                    case 4:
+                        FragmentTransaction transaction4 = getFragmentManager().beginTransaction();
+                        IllnessFogh fragment4 = new IllnessFogh();
+                        transaction4.replace(R.id.frame_layout, fragment4);
+                        transaction4.addToBackStack(null);
+                        transaction4.commit();
+                        break;
+
+
+                    case 5:
+                        FragmentTransaction transaction5 = getFragmentManager().beginTransaction();
+                        IllnessGhalb fragment5 = new IllnessGhalb();
+                        transaction5.replace(R.id.frame_layout, fragment5);
+                        transaction5.addToBackStack(null);
+                        transaction5.commit();
+                        break;
+
+
+                    case 6:
+
+                        FragmentTransaction transaction6 = getFragmentManager().beginTransaction();
+                        IllnessNorology fragment6 = new IllnessNorology();
+                        transaction6.replace(R.id.frame_layout, fragment6);
+                        transaction6.addToBackStack(null);
+                        transaction6.commit();
+                        break;
+
+                    case 7:
+                        FragmentTransaction transaction7 = getFragmentManager().beginTransaction();
+                        IllnessCCU fragment7 = new IllnessCCU();
+                        transaction7.replace(R.id.frame_layout, fragment7);
+                        transaction7.addToBackStack(null);
+                        transaction7.commit();
+                        break;
+
+                    case 8:
+                        FragmentTransaction transaction8 = getFragmentManager().beginTransaction();
+                        IllnessICU fragment8 = new IllnessICU();
+                        transaction8.replace(R.id.frame_layout, fragment8);
+                        transaction8.addToBackStack(null);
+                        transaction8.commit();
+                        break;
+
+                }
+
+
 
             }
+
+
+
         });
-
-        cheshm_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                IllnessEye fragment = new IllnessEye();
-                transaction.replace(R.id.frame_layout, fragment);
-                transaction.addToBackStack(null);
-                transaction.commit();
-
-            }
-        });
-
-        dakheli_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                IllnessDakheli fragment = new IllnessDakheli();
-                transaction.replace(R.id.frame_layout, fragment);
-                transaction.addToBackStack(null);
-                transaction.commit();
-
-            }
-        });
-
-        ofuni_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                IllnessOfuni fragment = new IllnessOfuni();
-                transaction.replace(R.id.frame_layout, fragment);
-                transaction.addToBackStack(null);
-                transaction.commit();
-
-            }
-        });
-
-        fogh_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                IllnessFogh fragment = new IllnessFogh();
-                transaction.replace(R.id.frame_layout, fragment);
-                transaction.addToBackStack(null);
-                transaction.commit();
-
-            }
-        });
-
-        ghalb_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                IllnessGhalb fragment = new IllnessGhalb();
-                transaction.replace(R.id.frame_layout, fragment);
-                transaction.addToBackStack(null);
-                transaction.commit();
-
-            }
-        });
-
-        norology_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                IllnessNorology fragment = new IllnessNorology();
-                transaction.replace(R.id.frame_layout, fragment);
-                transaction.addToBackStack(null);
-                transaction.commit();
-
-            }
-        });
-
-        ccu_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                IllnessCCU fragment = new IllnessCCU();
-                transaction.replace(R.id.frame_layout, fragment);
-                transaction.addToBackStack(null);
-                transaction.commit();
-
-            }
-        });
-
-        icu_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                IllnessICU fragment = new IllnessICU();
-                transaction.replace(R.id.frame_layout, fragment);
-                transaction.addToBackStack(null);
-                transaction.commit();
-
-            }
-        });
-
-
-
-
-
-
-
-
         return v;
     }
-
-
 }
