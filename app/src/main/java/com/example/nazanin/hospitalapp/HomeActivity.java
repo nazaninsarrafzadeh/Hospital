f7ee41bd71b5b263836ad4291afcb0ed20c2ca52
Fatima <sf.mirashrafi@gmail.com>
2018-09-23 09:21:10 +0330

    welcomePage added

---
+++ app/src/main/java/com/example/nazanin/hospitalapp/HomeActivity.java
@@ -0,0 +1,26 @@
+package com.example.nazanin.hospitalapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private static int splash_time_out = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(HomeActivity.this,MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+
+            }
+        },splash_time_out);
+    }
+}
